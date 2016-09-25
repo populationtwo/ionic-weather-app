10da24ca19c1481aa8207a097aafc335b03f1d3d
Edwin Lunandy <elunandy@newsignature.com>
2016-09-24 21:27:26 -0400

    Add Search view and controller.

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,23 @@
 angular.module('App', ['ionic'])
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    $stateProvider
+      .state('search', {
+        url: '/search',
+        controller: 'SearchController',
+        templateUrl: 'views/search/search.html'
+      });
+
+    $urlRouterProvider.otherwise('/search');
+  })
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
