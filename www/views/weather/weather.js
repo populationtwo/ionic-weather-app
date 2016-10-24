0151ba5f81504f660af1ea17066aab74d86517f6
Edwin Lunandy <elunandy@newsignature.com>
2016-10-23 22:46:33 -0400

    Update weather controller, calculate viewport height.

--- www/views/weather/weather.js
+++ www/views/weather/weather.js
@@ -1,9 +1,20 @@
 angular.module('App')
-.controller('WeatherController', function ($scope, $http, $stateParams, Settings) {
-  $scope.params = $stateParams;
-  $scope.settings = Settings;
+  .controller('WeatherController', function ($scope, $http, $stateParams, Settings) {
+    $scope.params = $stateParams;
+    $scope.settings = Settings;
 
-  $http.get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.lng, {params: {units: Settings.units}}).success(function (forecast) {
-    $scope.forecast = forecast;
+    $http.get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.lng, { params: { units: Settings.units } }).success(function (forecast) {
+      $scope.forecast = forecast;
+    });
+
+    var barHeight = document.getElementsByTagName('ion-header-bar')[0].clientHeight;
+    $scope.getWidth = function () {
+      return window.innerWidth + 'px';
+    };
+    $scope.getTotalHeight = function () {
+      return parseInt(parseInt($scope.getHeight()) * 3) + 'px';
+    };
+    $scope.getHeight = function () {
+      return parseInt(window.innerHeight - barHeight) + 'px';
+    };
   });
-});
